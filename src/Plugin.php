a6fa98dc77a515eabc22d5532204c0ca4a0e82fa
EdwardBock <edward.bock@palasthotel.de>
2021-04-21 10:58:05 +0200

    abstract plugin class

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,9 +8,9 @@ namespace Palasthotel\WordPress;
  * @property string path
  * @property string url
  */
-class Plugin {
+abstract class Plugin {
 
-	private function __construct() {
+	public function __construct() {
 		$this->path = plugin_dir_path(__FILE__);
 		$this->url = plugin_dir_url(__FILE__);
 
@@ -19,7 +19,7 @@ class Plugin {
 	private static $instance;
 	public static function instance(){
 		if(!self::$instance){
-			self::$instance = new self();
+			self::$instance = new static();
 		}
 		return self::$instance;
 	}
