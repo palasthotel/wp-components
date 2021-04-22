b419bad2b7812cb7ac863d90455df386be40a9a3
EdwardBock <edward.bock@palasthotel.de>
2021-04-22 09:54:14 +0200

    lifecycle methods

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,24 +11,30 @@ namespace Palasthotel\WordPress;
 abstract class Plugin {
 
 	public function __construct() {
-		$this->path = plugin_dir_path(__FILE__);
-		$this->url = plugin_dir_url(__FILE__);
+		$this->path = plugin_dir_path( __FILE__ );
+		$this->url  = plugin_dir_url( __FILE__ );
+
+		$this->onCreate();
+
+		register_activation_hook( __FILE__, array( $this, "onActivation" ) );
+		register_deactivation_hook( __FILE__, array( $this, "onDeactivation" ) );
 	}
 
-	public function loadTextDomain($domain){
-		load_plugin_textdomain(
-			$domain,
-			false,
-			dirname( plugin_basename( __FILE__ ) ) . '/languages'
-		);
+	abstract function onCreate();
+
+	function onActivation() {
 	}
 
+	function onDeactivation() {
+	}
 
 	private static $instance;
-	public static function instance(){
-		if(!self::$instance){
+
+	public static function instance() {
+		if ( ! self::$instance ) {
 			self::$instance = new static();
 		}
+
 		return self::$instance;
 	}
 }
