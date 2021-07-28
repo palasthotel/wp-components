a12f08bd5200652e252548ea3d8304fdcd1dbe43
EdwardBock <edward.bock@palasthotel.de>
2021-07-28 10:36:25 +0200

    select meta field update

--- src/Attachment/SelectMetaField.php
+++ src/Attachment/SelectMetaField.php
@@ -5,11 +5,12 @@ namespace Palasthotel\WordPress\Attachment;
 
 
 use Palasthotel\WordPress\Model\Option;
+use Palasthotel\WordPress\Service\ProviderInterface;
 
 /**
  * Class SelectMetaField
  * @package Palasthotel\WordPress\Attachment
- * @version 0.1.1
+ * @version 0.2.0
  */
 class SelectMetaField extends MetaField {
 
@@ -19,11 +20,11 @@ class SelectMetaField extends MetaField {
 	private $options = [];
 
 	/**
-	 * @param Option[] $options
+	 * @param Option[]|ProviderInterface $options
 	 *
 	 * @return $this
 	 */
-	public function options( array $options ): self {
+	public function options( $options ): self {
 		$this->options = $options;
 
 		return $this;
@@ -36,12 +37,14 @@ class SelectMetaField extends MetaField {
 		$name = $this->getFormName($post->ID);
 		$value = $this->getValue($post->ID);
 
+		$options = $this->options instanceof ProviderInterface ? $this->options->get() : $this->options;
+
 		ob_start();
 		echo "<select name='$name' id='attachments-{$post->ID}-{$this->id}' style='max-width: 100%'>";
-		foreach ($this->options as $option){
-		    $selected = ($value === $option->value) ? "selected='selected'" : "";
-            echo "<option value='$option->value' $selected>$option->label</option>";
-        }
+		foreach ($options as $option){
+			$selected = ($value === $option->value) ? "selected='selected'" : "";
+			echo "<option value='$option->value' $selected>$option->label</option>";
+		}
 		echo "</select>";
 		$field["html"] = ob_get_contents();
 		ob_end_clean();
